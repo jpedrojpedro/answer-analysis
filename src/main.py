46ef4ce679f657f3897d69cbdd19db68a02898d0
João Pedro Pinheiro <joaopedro.pinheiro88@gmail.com>
2021-02-17 01:05:08 -0300

    Allowing specific question selection and few adjusts

--- src/main.py
+++ src/main.py
@@ -3,32 +3,41 @@ from src.graph_analysis.graph_statistics import GraphStatistics
 from src.enrichment.tabulate import Tabulate
 from src.enrichment.ranking import Ranking
 from src.enrichment.frequency import Frequency
-from src.enrichment.new_question import NewQuestion
+from src.enrichment.new_query import NewQuery
 
 
 # TODO: implement like Rake-Rails
 class Main:
     def __init__(self, dataset='brainz.json'):
-        self.dataset = dataset
+        self.dataset = Dataset(dataset)
 
     def run(self):
-        dataset = Dataset(self.dataset)
-        dataset.parse()
-        endpoint = dataset.endpoint
-        gs = GraphStatistics(endpoint, dataset, dest_folder='./src/graph_analysis/' + dataset.name)
-        gs.run(load=False)
-        for idx, question in enumerate(dataset.questions, start=1):
-            print("----------{}----------".format(idx))
-            print("Question: {}".format(question.question))
-            tabulate = Tabulate(endpoint, question)
-            dft = tabulate.apply()
-            frequency = Frequency(dft, gs.predicates, dataset.uri_inforank)
-            dff = frequency.apply()
-            new_question = NewQuestion(dft, dff)
-            new_question.generate()
-            # TODO: step ranking only called when no new question can be formulated
-            ranking = Ranking(dft, dataset.uri_inforank)
-            dfr = ranking.apply()
+        self.dataset.parse()
+        endpoint = self.dataset.endpoint
+        gs = GraphStatistics(endpoint, self.dataset, dest_folder='./src/graph_analysis/' + self.dataset.name)
+        gs.run(load=True)
+        question = self.selection_prompt()
+        print("Question: {}".format(question.question))
+        tabulate = Tabulate(endpoint, question)
+        dft = tabulate.apply()
+        print(tabulate.enriched_query)
+        frequency = Frequency(dft, gs.predicates, self.dataset.uri_inforank)
+        dff = frequency.apply()
+        new_query = NewQuery(dft, dff, tabulate.enriched_query, threshold=10)
+        new_query.generate()
+        # TODO: step ranking only called when no new question can be formulated
+        ranking = Ranking(dft, self.dataset.uri_inforank)
+        dfr = ranking.apply()
+        print(dfr)
+
+    def selection_prompt(self):
+        print("---------------------")
+        options = {idx: question for idx, question in enumerate(self.dataset.questions, start=1)}
+        for idx, question in options.items():
+            print("{} - {}".format(idx, question.question))
+        print("---------------------")
+        id_selected = input("Selecione uma das peguntas acima (7):\t") or '7'
+        return options[int(id_selected)]
 
 
 if __name__ == '__main__':
