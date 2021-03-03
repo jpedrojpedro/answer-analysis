3d0fac483aabd780746ee72925963f5e65fe8f57
João Pedro Pinheiro <joaopedro.pinheiro88@gmail.com>
2021-03-03 16:57:34 -0300

    Allowing Ranking customization with sort and top (limit) as params

--- src/flow/ranking.py
+++ src/flow/ranking.py
@@ -6,12 +6,13 @@ class Ranking:
         self.df = df_tabulated
         self.uri_inforank = uri_inforank
 
-    def apply(self):
+    def apply(self, sort='asc', top=10):
         df_rank = self.df.where(self.df['predicate'] == self.uri_inforank)
         df_rank = df_rank.dropna()
         # TODO: improve performance .lower()
         df_rank['object'] = df_rank['object'].str.lower()
         df_rank['object'] = df_rank['object'].str.replace(',', '.')
         df_rank['object'] = pd.to_numeric(df_rank['object'])
-        df_rank = df_rank.sort_values('object', ascending=False)
+        df_rank = df_rank.sort_values('object', ascending=True if sort == 'asc' else False)
+        df_rank = df_rank.iloc[:top]
         return df_rank
